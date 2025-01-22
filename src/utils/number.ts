f483aba4a39354b7183445f79d1577ff070acc6d
Oscar Alves <osk.alves@gmail.com>
2025-01-22 18:44:13 -0300

    chore: update package.json

---
+++ src/utils/number.ts
@@ -0,0 +1,9 @@
+export function formatNumberIntl(value: number): string {
+  const formatter = new Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    compactDisplay: 'short',
+    maximumFractionDigits: 1,
+  });
+
+  return formatter.format(value);
+}
